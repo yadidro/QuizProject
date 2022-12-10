193f8ff45951fce2a78013c3e2ca97ae5fd084a5
GLOBAL\C5322996 <roy.yadid@sap.com>
2022-12-11 00:57:31 +0200

    remove using

--- BackendSide/Controllers/QuestionsController.cs
+++ BackendSide/Controllers/QuestionsController.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using BackendSide.Models;
 using BackendSide.Repositories;
 using Microsoft.AspNetCore.Mvc;
